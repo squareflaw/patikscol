ed19998d75de61fd06421057476cd5f6b8c1d7aa
Albin <squareflaw@gmail.com>
2018-08-01 20:19:26 -0400

    Builds the mobile version of the landing page and the main illustrations
    -Adds new illustrations created in Illustrator
    -Changes the main font to Lobster
    -fix the menu button color on changes
    -sets a temporary footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 (function() {
 
+const header = document.querySelector('.header');
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
@@ -10,9 +11,21 @@ const NavItems = document.querySelectorAll('.menu-nav__item');
 
 menuBtn.addEventListener('click', function(e) {    
     menuBtn.classList.toggle('isClosed');
+    header.classList.toggle('isShown');    
     menu.classList.toggle('isShown');
     menuNav.classList.toggle('isShown');
     NavItems.forEach(item => item.classList.toggle('isShown'));
+});
+
+window.addEventListener('scroll', function(e) {
+    let onTop = window.scrollY;
+    if (!onTop) {
+      header.classList.remove('scroll');
+      menuBtn.classList.remove('scroll');
+    }else {
+      header.classList.add('scroll');
+      menuBtn.classList.add('scroll');
+    }
 });
 
 
