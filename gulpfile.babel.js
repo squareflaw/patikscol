c41c3ceea1af6343b56d8a829e0189579f74f810
Albin <squareflaw@gmail.com>
2018-08-02 19:15:55 -0400

    Completes all the html of the home page
    -adds the schema metadata
    -adds the pets photos
    -Inserts an instagram post from the organization's profile
    -adds the footer icons
    -changes the gulp file to refresh the browser faster

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,10 @@
-// import gulp from 'gulp';
-// import plumber from 'gulp-plumber';
-// import pug from 'gulp-pug';
-// import browserSync from 'browser-sync';
-// import sass from 'gulp-sass';
-// import browserify from 'browserify';
+
 
 const gulp = require( 'gulp');
 const plumber = require( 'gulp-plumber');
 const pug = require( 'gulp-pug');
 const browserSync = require( 'browser-sync');
+const reload      = browserSync.reload; 
 const sass = require( 'gulp-sass');
 const postcss = require( 'gulp-postcss');
 const cssnano = require( 'cssnano');
@@ -23,9 +19,13 @@ const pngcrush = require( 'imagemin-pngcrush');
 const notify = require( 'gulp-notify');
 const data = require( 'gulp-data');
 const fs = require( 'fs');
+const runSequence = require('run-sequence');
 
 
-const server = browserSync.create();
+const bases = {
+    app:  'src/',
+    dist: 'public/',
+};
 
 const postcssPlugins = [
   cssnano({
@@ -37,6 +37,16 @@ const postcssPlugins = [
   })
 ];
 
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Failure!",
+    message:  "Error: <%= error.message %>",
+    sound:    "Basso"
+  })(err);
+  this.emit('end');
+};
+
 const sassOptions = {
 	outputSyle: 'expanded',
 	includePaths: ['node_modules']
@@ -45,22 +55,23 @@ const sassOptions = {
 gulp.task('styles', () =>
   gulp.src('./src/scss/styles.scss')
     .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(plumber())
+    .pipe(plumber({errorHandler: onError}))
     .pipe(sass(sassOptions))
     .pipe(postcss(postcssPlugins))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./public/css'))
-    .pipe(server.stream({match: '**/*.css'}))
+    .pipe(reload({stream:true}))
 );
 
 gulp.task('pug', () =>
   gulp.src('./src/pug/pages/*.pug')
-    .pipe(plumber())
+    .pipe(plumber({errorHandler: onError}))
     // .pipe(data(function(file) {
     //   return JSON.parse(fs.readFileSync('./src/data/casos-de-exito.json'))
     // }))
     .pipe(pug())
     .pipe(gulp.dest('./public'))
+    .pipe(reload({stream:true}))
 );
 
 gulp.task('scripts', () =>
@@ -76,6 +87,7 @@ gulp.task('scripts', () =>
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./public/js'))
+    .pipe(reload({stream:true}))
 );
 
 gulp.task('images', function() {
@@ -86,30 +98,44 @@ gulp.task('images', function() {
     use: [pngcrush()]
   }))
   .pipe(gulp.dest('./public/img'))
+  .pipe(reload({stream:true}))
   .pipe(notify("Finished images task!"));
 });
 
 
 gulp.task('copy', function() {
- gulp.src('./src/img/**/*.svg')
+ gulp.src('./src/img/**/*')
   .pipe(gulp.dest('./public/img'))
+  .pipe(reload({stream:true}))
 });
 
-gulp.task('default', ['styles', 'pug', 'scripts', 'images', 'copy'], () => {
-  server.init({
+
+
+gulp.task('browser-sync', function() {
+  browserSync({
     server: {
-      baseDir: './public'
-    },
-  },);
-
-  // imagenes
-  gulp.watch('./src/img/**/*', ['images']);
-  gulp.watch('./src/img/**/*.svg', ['copy']);
-
-  watch('./src/scss/**/*.scss', () => gulp.start('styles'));
-  watch('./src/js/**/*.js', () => gulp.start('scripts',server.reload) );
-  watch('./src/data/**/*.json','./src/pug/**/*.pug', () => gulp.start('pug', server.reload) );
-  watch('./src/pug/pages/*.pug', () => gulp.start('pug', server.reload) );
-  watch('./src/img/**/*.{png,jpg,jpeg,gif}', () => gulp.start('images') );
-  watch('./src/img/**/*.svg', () => gulp.start('copy') );
+      baseDir: bases.dist
+    }
+  });
 });
+
+gulp.task('watch', function() {
+  gulp.watch(bases.app + 'scss/**/*.scss', ['styles']);
+  gulp.watch(bases.app + 'pug/**/*.pug', ['pug']);
+  gulp.watch(bases.app + 'img/*', ['copy']);
+  gulp.watch(bases.app + 'js/*', ['scripts']);
+});
+
+
+
+
+
+gulp.task('default', function(done) {
+  runSequence('browser-sync', 'styles', 'pug', 'scripts', 'copy', 'watch', done);
+});
+
+gulp.task('deploy', function(done) {
+  runSequence('styles', 'pug', 'scripts', 'copy', 'images', 'watch', done);
+});
+
+
